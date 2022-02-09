f8aea374779b8c3b7939efe9183e47058335e23b
Luiz Glomyer <glomyerjunior@hotmail.com>
2022-02-09 11:26:12 -0400

    Fixing Github profile

--- teste/teste.js
+++
@@ -1,9 +0,0 @@
-function mexer() {
-  var color = document.getElementById('a').style.backgroundColor;
-  if (color == "black") {
-    document.getElementById('a').style.backgroundColor = "red";
-  }
-  else
-    document.getElementById('a').style.backgroundColor = "black";
-}
-
